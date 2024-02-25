b643d127168f2540bb93a85e81c381a54e537d6a
Raul_Canton <raulcantong@hotmail.com>
2024-02-25 11:33:48 +0100

    Clase CostePersonal.java refactorizada_2

--- CostePersonal.java
+++ CostePersonal.java
@@ -1,7 +1,9 @@
 // Falta importar la clase Trabajador
+
 public class CostePersonal {
     //Creo una constante para las horas extra inicializada a 20
     private static final int TOTAL_HORAS= 20;
+
     // Método para sumar las horas extras a la nómina.
    private static float calculoHorasExtras(Trabajador trabajador) {
         return trabajador.getNomina() + (trabajador.getHorasExtras() + TOTAL_HORAS);
@@ -9,10 +11,12 @@ public class CostePersonal {
     static float CosteDelPersonal(Trabajador trabajadores[]) {
         float costeFinal = 0;
         Trabajador trabajador;
+
         // Se cambia la "y" por la variable "i" 
         for (int i = 0; i < trabajadores.length; i++) {
             trabajador = trabajadores[i];
             if (trabajador.getTipoTrabajador() == Trabajador.DIRECTOR|| trabajador.getTipoTrabajador() == Trabajador.SUBDIRECTOR) {
+
                 // Se quita el simbolo de +
                 costeFinal = trabajador.getNomina();
             }
